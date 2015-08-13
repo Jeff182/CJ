e103e9e1514a24a832d7c722ba9c7f7e96d03224
nsato <nobuosato@gmail.com>
2015-08-13 10:43:55 -0400

    update

--- sharespace/codes/F2d_F2.py
+++ sharespace/codes/F2d_F2.py
@@ -16,36 +16,90 @@ class F2ratio(object):
 
   def __init__(self):
     self.load_data()
-    self.make_plot()
+    self.make_plotI()
+    self.make_plotII()
 
-  def load_data(self):
-    F=open('../CJ15data/FITS_150701_LO_NLO/calc_NLO_KP_AV18.out')
+  def _load_data(self,name):
+    D=self.D
+    F=open('../CJ15data/FITS_150701_LO_NLO/calc_NLO_KP_'+name+'.out')
     L=F.readlines()
     F.close()
     L=[l.strip() for l in L]
     L=[l for l in L if l!='']
-    p=[l.split() for l in L if 'slac_p_reb' in l if len(l.split())<25]
-    d=[l.split() for l in L if 'slac_d_reb' in l if len(l.split())<25]
+    data=[l.replace('*','').split() for l in L if 'test_DN' in l if len(l.split())<25]
     for l in L:
-      if all([k in l for k in ['X','Q2','W2','THEORY']]): H=l.replace('_w','').split()
-   
-    D={}
-    D['p']=pd.DataFrame(p,columns=H)
-    D['d']=pd.DataFrame(d,columns=H)
-    D['p']=D['p'].convert_objects(convert_numeric=True)
-    D['d']=D['d'].convert_objects(convert_numeric=True)
-    self.D=D
+      if all([k in l for k in ['X','Q2','W2','THEORY']]):
+        H=l.replace('_w','').replace(r'chi^2','').split()
+    DF=pd.DataFrame(data,columns=H)
+    DF=DF.convert_objects(convert_numeric=True)
+    D[name]=DF
 
-  def make_plot(self):
+  def load_data(self):
+    self.D={}
+    self._load_data('AV18')
+    self._load_data('CDBONN')
+    self._load_data('WJC1')
+    self._load_data('WJC2')
+
+  def make_plotI(self):
     # retrieve data
     D=self.D
-    Dp=D['p']
-    Dd=D['d']
+
+    kmap={}
+    kmap['AV18']   = {'c':'r','ls':'-'}
+    kmap['CDBONN'] = {'c':'g','ls':'--'}  
+    kmap['WJC1']   = {'c':'b','ls':'-.'}
+    kmap['WJC2']   = {'c':'k','ls':':'}
+
     ax=py.subplot(111)
-    ax.plot(Dp.X,Dp.Q2,'r.')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Q2')
-    py.savefig('gallery/tmp_XQ.pdf')
+    for k in D.keys():
+      DF=D[k]
+      DF=DF[DF.Q2==10]
+      cls=kmap[k]['c']+kmap[k]['ls']
+      ax.plot(DF.X,DF.THEORY,cls,label=tex(k))
+
+    ax.set_xlabel('$x$',size=25)
+    ax.set_ylabel(r'$F_2(D)/F_2(n+p)$',size=20)
+    ax.set_ylim(0.95,1.1)
+    ax.axhline(1,color='k',ls=':',alpha=0.5)
+
+    ax.legend(frameon=0,loc=2,fontsize=20)
+    py.tick_params(axis='both',labelsize=20)
+    py.tight_layout()
+    py.savefig('gallery/F2d_F2_I.pdf')
+    py.close()
+
+  def make_plotII(self):
+    # retrieve data
+    D=self.D
+
+    kmap={}
+    kmap['Q2 = 2']   = {'c':'r','ls':'-'}
+    kmap['Q2 = 5']   = {'c':'g','ls':'--'}
+    kmap['Q2 = 10']  = {'c':'b','ls':'-.'}
+    kmap['Q2 = 50']  = {'c':'k','ls':':'}
+    kmap['Q2 = 100'] = {'c':'k','ls':'-'}
+
+
+
+    ax=py.subplot(111)
+    DF=D['AV18']
+    for Q2 in [2,5,10,50,100]:
+      k='Q2 = %d'%Q2
+      Q2=float(k.split('=')[1])
+      DF=D['AV18'][D['AV18'].Q2==Q2]
+      cls=kmap[k]['c']+kmap[k]['ls']
+      ax.plot(DF.X,DF.THEORY,cls,label=tex('AV18~')+r'$Q^2=%0.0f~GeV^2$'%Q2)
+
+    ax.set_xlabel('$x$',size=25)
+    ax.set_ylabel(r'$F_2(D)/F_2(n+p)$',size=20)
+    ax.set_ylim(0.95,1.1)
+    ax.axhline(1,color='k',ls=':',alpha=0.5)
+
+    ax.legend(frameon=0,loc=2,fontsize=20)
+    py.tick_params(axis='both',labelsize=20)
+    py.tight_layout()
+    py.savefig('gallery/F2d_F2_II.pdf')
 
 if __name__=='__main__':
 
